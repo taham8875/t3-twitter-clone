cf79db058cf51f29992fad9a4910796e502346ab
taham8875 <taha.m8875@gmail.com>
2023-07-15 18:06:46 +0300

    set github auth provider

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,12 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
   },
 };
 
